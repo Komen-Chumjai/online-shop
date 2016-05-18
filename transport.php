f27241f1634a4bb35b0a1043b3e529d2fe7cb81b
Sorapongdpu <550107030009@dpu.ac.th>
2016-05-18 23:11:57 +0700

    Add files via upload

---
+++ transport.php
@@ -0,0 +1,32 @@
+<table align="center" width="100%" height="484" style="background-color:white">
+  <tr>
+    <td colspan="3" bgcolor="#FFCC66"><h1><center>Transport</center></h1></td>
+  </tr>
+  <tr>
+    <td colspan="3" class="james"><b>ลูกค้าจะได้รับสินค้าภายใน 48 ชั่วโมง</b></td>
+  </tr>
+  <tr>
+    <td colspan="3">&nbsp;</td>
+  </tr>
+  <tr>
+    <td width="252"><center><img src="image/transport1.jpg" width="148" height="138" /></center></td>
+    <td colspan="2"><p><b>แจ้งโอนเงินก่อน 13.00 น. [วันจันทร์ - ศุกร์]</b></p>
+    <p><b>จัดส่งในวันนั้นทันที</b></p></td>
+  </tr>
+  <tr>
+    <td><center><img src="image/transport2.jpg" width="145" height="137" /></center></td>
+    <td colspan="2"><p><b>แจ้งโอนเงินหลัง 13.00 น. [วันจันทร์ - พฤหัสบดี]</b></p>
+    <p><b>จัดส่งสินค้าในวันถัดไป</b></p></td>
+  </tr>
+  <tr>
+    <td><center><img src="image/transport3.jpg" width="155" height="137" /></center></td>
+    <td colspan="2"><p><b>แจ้งโอนเงินหลัง 13.00 น. [วันศุกร์]</b></p>
+    <p><b>จัดส่งสินค้าวันจันทร์</b></p></td>
+  </tr>
+  <tr>
+    <td><center><img src="image/transport4.jpg" width="162" height="130" /></center></td>
+    <td colspan="2"><p><b>แจ้งโอนเงินภายในวัน เสาร์ - อาทิตย์</b></p>
+    <p><b>จัดส่งสินค้าวันจันทร์</b></p></td>
+  </tr>
+</table>
+<?php //<img src="image/transport1" width="148" height="138" /> ?>
